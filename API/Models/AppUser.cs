b963bce8bf5a65106fb3a2536b2f40ed3fee85ff
Reza Taba <mrtabaa@gmail.com>
2021-08-18 22:00:21 +0430

    added props

--- API/Models/AppUser.cs
+++ API/Models/AppUser.cs
@@ -9,5 +9,7 @@ namespace API.Models {
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
     }
 }
