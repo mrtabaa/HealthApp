9935646e68002b410f6d3b7497513708e3c396c7
Reza Taba <mrtabaa@gmail.com>
2021-08-18 22:00:11 +0430

    Created DTOs

--- API/DTOs/LoginDto.cs
+++ API/DTOs/LoginDto.cs
@@ -1,12 +1,10 @@
 using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs {
-    public class RegisterDto {
+    public class LoginDto {
         [Required]
         public string Username { get; set; }
         [Required]
         public string Password { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
     }
 }
