f2aa8b1edcaaddc89094d94004f4ce99529ac573
Reza Taba <mrtabaa@gmail.com>
2021-08-18 16:38:03 +0430

    Moved required validation to RegisterDto

--- API/Models/AppUser.cs
+++ API/Models/AppUser.cs
@@ -5,13 +5,9 @@ namespace API.Models {
     public record AppUser {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        [BsonRequired]
         public string Id { get; init; }
-        [BsonRequired]
         public string Username { get; set; }
-        [BsonRequired]
         public byte[] PasswordHash { get; set; }
-        [BsonRequired]
         public byte[] PasswordSalt { get; set; }
     }
 }
