8270efb25bc823f50105ff028c8eb876452b7a4b
Reza Taba <mrtabaa@gmail.com>
2021-08-23 21:58:01 +0430

    flex-layout installation

--- client/src/app/modules/component.module.ts
+++ client/src/app/modules/component.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from '../app-routing.module';
 import { MaterialModule } from './material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 import { HomeComponent } from '../components/home/home.component';
 import { LoginComponent } from '../components/login/login.component';
@@ -24,7 +25,8 @@ const components = [
     CommonModule,
     AppRoutingModule,
     MaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   exports: [components]
 })
