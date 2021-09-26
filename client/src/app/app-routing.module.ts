cb3d1ff5c7b9f63b32f6e1f155803a976594f242
Reza Taba <mrtabaa@gmail.com>
2021-09-26 20:01:31 +0330

    set routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NoAccessComponent } from './components/no-access/no-access.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  // { path: 'no-access', component: NoAccessComponent, canActivate: [AuthGuard] },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
