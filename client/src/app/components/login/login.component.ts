cf4564be6304ce35d673a8613326df1fcf28ff5e
Reza Taba <mrtabaa@gmail.com>
2021-09-26 20:15:31 +0330

    set login page structure

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -8,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
+  // constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+
+  loginGoogle(): void {
+    // this.authService.login(new firebase.default.auth.GoogleAuthProvider());
+  }
+
+  loginFacebook(): void {
+    // this.authService.login(new firebase.default.auth.FacebookAuthProvider());
+  }
+
+  loginEmail(): void {
+    // this.authService.login(new firebase.default.auth.EmailAuthProvider());
   }
 
 }
