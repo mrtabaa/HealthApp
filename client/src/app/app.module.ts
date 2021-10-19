42d6a2cef2872833e30fb411cddb4a726a3de524
Reza Taba <mrtabaa@gmail.com>
2021-10-19 04:56:49 +0330

    re-installed

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,24 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { ComponentModule } from './modules/component.module';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    HttpClientModule,
-
-    ComponentModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
