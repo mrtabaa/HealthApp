407d8f54f8684d01db15a185d276f6c3fdb56415
Reza Taba <mrtabaa@gmail.com>
2021-10-30 01:11:14 +0300

    lazi loading all images

--- client/src/app/modules/component.module.ts
+++ client/src/app/modules/component.module.ts
@@ -7,6 +7,9 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
 
 // directives
+import { LazyImgDirective } from '../directives/lazy-img.directive';
+import { InputFormatDirective } from '../directives/input-format.directive';
+import { ClearControlFieldByClick } from '../directives/clear-control-field-by-click.directive';
 
 // components
 import { HomeComponent } from '../components/home/home.component';
@@ -15,12 +18,11 @@ import { LoginComponent } from '../components/login/login.component';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 import { NoAccessComponent } from '../components/no-access/no-access.component';
 import { NotFoundComponent } from '../components/not-found/not-found.component';
-import { InputFormatDirective } from '../directives/input-format.directive';
-import { ClearControlFieldByClick } from '../directives/clear-control-field-by-click.directive';
 
 const components = [
   InputFormatDirective,
   ClearControlFieldByClick,
+  LazyImgDirective,
 
   HomeComponent,
   SignupLabComponent,
