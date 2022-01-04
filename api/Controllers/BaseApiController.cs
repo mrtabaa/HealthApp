27b95ad0ed03a04d13742d1bb08ccd54e1ecf727
Reza Taba <mrtabaa@gmail.com>
2022-01-05 02:38:16 +0300

    set BaseController attributes + more usings

---
+++ api/Controllers/BaseApiController.cs
@@ -0,0 +1,9 @@
+namespace api.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+// [Authorize]
+//use ControllerBase for api
+//use Controller     for Views and/or api
+public class BaseApiController : ControllerBase {
+
+}
--- api/GlobalUsing.cs
+++ api/GlobalUsing.cs
@@ -1,3 +1,10 @@
 global using API.Settings;
 global using Microsoft.Extensions.Options;
 global using MongoDB.Driver;
+global using API.Models;
+global using MongoDB.Bson;
+global using MongoDB.Bson.Serialization.Attributes;
+global using Microsoft.AspNetCore.Mvc;
+global using api.Repositories;
+global using Microsoft.AspNetCore.Authorization;
+
