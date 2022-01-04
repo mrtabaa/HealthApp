46e4532426498a10576427d2e94951325c6dcb8f
Reza Taba <mrtabaa@gmail.com>
2022-01-04 20:40:06 +0300

    fixed Nullable flags

--- api/models/AppUser.cs
+++ api/models/AppUser.cs
@@ -2,14 +2,13 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace API.Models;
-public record AppUser
-{
+public record AppUser {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; init; }
-    public string Username { get; set; }
-    public byte[] PasswordHash { get; set; }
-    public byte[] PasswordSalt { get; set; }
-    public string Email { get; set; }
-    public string Phone { get; set; }
+    public string? Id { get; init; }
+    public string? Username { get; set; }
+    public byte[]? PasswordHash { get; set; }
+    public byte[]? PasswordSalt { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
 }
