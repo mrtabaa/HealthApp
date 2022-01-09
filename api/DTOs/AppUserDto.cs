5ca8991bf6c6a19a95c1799a9c0c922d303e50e3
Reza Taba <mrtabaa@gmail.com>
2022-01-09 20:54:32 +0300

    Created AppUserDto

---
+++ api/DTOs/AppUserDto.cs
@@ -0,0 +1,8 @@
+namespace api.DTOs;
+public class AppUserDto {
+    public string? id { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+}
