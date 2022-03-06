df0c84f7f7c1f451fd7f53cdbcb97defed39a4e2
Reza Taba <mrtabaa@gmail.com>
2022-03-06 21:01:32 +0300

    cleaning

--- api/Controllers/BaseApiController.cs
+++ api/Controllers/BaseApiController.cs
@@ -1,7 +1,6 @@
 namespace api.Controllers;
 [ApiController]
 [Route("[controller]")]
-// [Authorize]
 //use ControllerBase for api
 //use Controller     for Views and/or api
 public class BaseApiController : ControllerBase {
