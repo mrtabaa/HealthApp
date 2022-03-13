f1129902fcf6dd52c05613d0c1820bb190713215
Reza Taba <mrtabaa@gmail.com>
2022-03-13 22:55:57 +0300

    format code

--- client/src/app/modules/directive.module.ts
+++ client/src/app/modules/directive.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
-import { AutofocusDirective } from '../directives/autofocus.directive';
 import { CheckCountryExistsDirective } from '../directives/check-country-exists.directive';
-import { ClearControlFieldByClick } from '../directives/clear-control-field-by-click.directive';
-import { FocusNextDirective } from '../directives/focus-next.directive';
+import { ClearControlFieldByClickDirective } from '../directives/clear-control-field-by-click.directive';
 import { InputFormatDirective } from '../directives/input-format.directive';
 import { LazyImgDirective } from '../directives/lazy-img.directive';
+import { AutofocusDirective } from '../directives/autofocus.directive';
+import { FocusNextDirective } from '../directives/focus-next.directive';
 
 const directives: any[] = [
   InputFormatDirective,
-  ClearControlFieldByClick,
-  LazyImgDirective,
   CheckCountryExistsDirective,
+  ClearControlFieldByClickDirective,
+  LazyImgDirective,
   AutofocusDirective,
   FocusNextDirective,
 ]
