3ee34f77b296bf024e3a0b73a1ffc1af640ef971
Reza Taba <mrtabaa@gmail.com>
2022-07-06 19:59:53 +0200

    Update ApplicationServiceExtensions.cs
    moved DatabaseName from Repo to Extension

--- api/Extensions/ApplicationServiceExtensions.cs
+++ api/Extensions/ApplicationServiceExtensions.cs
@@ -3,12 +3,19 @@ namespace api.Extensions;
 public static class ApplicationServiceExtensions {
 
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config) {
-        // MongoDbSettings
+
+        #region MongoDbSettings            
         services.Configure<MongoDbSettings>(config.GetSection(nameof(MongoDbSettings)));
+
+        services.AddSingleton<IMongoDbSettings>(sp =>
+        sp.GetRequiredService<IOptions<MongoDbSettings>>().Value);
+
         services.AddSingleton<IMongoClient>(serviceProvider => {
             var uri = serviceProvider.GetRequiredService<IOptions<MongoDbSettings>>().Value;
             return new MongoClient(uri.ConnectionString);
         });
+        #endregion MongoDbSettings
+
 
         return services;
     }
